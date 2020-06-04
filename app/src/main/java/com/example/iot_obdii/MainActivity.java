254f3ce08ca0f294162e879e672b911b9ccef4c6
Teyhan <teyhanytu@gmail.com>
2020-06-04 23:12:46 +0300

    Voltaga Added, String to int

--- app/src/main/java/com/example/iot_obdii/MainActivity.java
+++ app/src/main/java/com/example/iot_obdii/MainActivity.java
@@ -19,9 +19,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-    MainActivity m = this;
+    MainActivity activity = this;
     EditText  speedText = null;
     EditText  rpmText = null;
+    EditText voltage;
     protected  ArrayList<Integer> buffer = new ArrayList<Integer>();
 
     @Override
@@ -29,51 +30,47 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        speedText = (EditText) findViewById(R.id.editText1);
-        rpmText = (EditText) findViewById(R.id.editText2);
-        Button button = (Button) findViewById(R.id.button1);
-
-        button.setOnClickListener(new OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Log.i(DISPLAY_SERVICE, "Button clicked : " + v.getId());
-                init();
-            }
-        });
+        speedText = findViewById(R.id.editText1);
+        rpmText = findViewById(R.id.editText2);
+        voltage = findViewById(R.id.voltage);
     }
 
-    public  void setSpeed(String mspeed){
-        speedText.setText(mspeed);
-    }
-
-    public  void setRPM(String rpm){
-        rpmText.setText(rpm);
-    }
-
-    public void init(){
+    public void connectBTN(View view){
+        Log.i(DISPLAY_SERVICE, "Connection button clicked : " + view.getId());
         //Context context = this.getApplicationContext();
-        Ytask task = new Ytask(m);
+        Ytask task = new Ytask(activity);
         task.execute("");
     }
 
-    private class Ytask extends AsyncTask<String, String, String> {
+    public void setSpeed(Integer speed){
+        speedText.setText(speed.toString());
+    }
+
+    public  void setRPM(Integer rpm){
+        rpmText.setText(rpm.toString());
+    }
+    public void setVoltage(Integer volt){
+        voltage.setText(volt.toString());
+    }
+
+    private class Ytask extends AsyncTask<String, Integer, String> {
 
         MainActivity main;
 
         public  Ytask(MainActivity main){
-
             this.main = main;
         }
 
         @Override
-        protected void onProgressUpdate(String... params) {
+        protected void onProgressUpdate(Integer... params) {
 
-            if(params[1].equalsIgnoreCase("speed"))
+            if(params[1] == 0)
                 this.main.setSpeed(params[0]);
 
-            if(params[1].equalsIgnoreCase("rpm"))
+            if(params[1] == 1)
                 this.main.setRPM(params[0]);
+            if(params[1] == 2)
+                this.main.setVoltage(params[0]);
         }
 
         @Override
@@ -82,14 +79,24 @@ public class MainActivity extends Activity {
                 Socket   wSocket = new Socket("192.168.0.10",35000);
                 while (true){
                     sendCmd(wSocket,"01 0D");
-                    String value = readSpeedData(wSocket,1);
+                    Integer speedData = readSpeedData(wSocket,1);
                     // this.main.setSpeed("mspeedewrere");
-                    publishProgress(value,"Speed");
+                    publishProgress(speedData,0); // 0 Speed
 
                     sendCmd(wSocket,"01 0C");
-                    String valuestr = readRPMData(wSocket,1);
+                    int RPMData = readRPMData(wSocket,1);
                     // this.main.setSpeed("mspeedewrere");
-                    publishProgress(valuestr,"rpm");
+                    publishProgress(RPMData,1);  // 1 RPM
+
+                    sendCmd(wSocket,"atrv");
+                    int voltageData = readVoltageData(wSocket,1);
+                    // this.main.setSpeed("mspeedewrere");
+                    publishProgress(voltageData,2); // 2 voltage
+/*
+                    sendCmd(wSocket,"01 05");
+                    String coolantData = readRPMData(wSocket,1);
+                    // this.main.setSpeed("mspeedewrere");
+                    publishProgress(coolantData,"coolant");*/
                 }
                 //}
             }   catch (Exception e) {
@@ -98,17 +105,9 @@ public class MainActivity extends Activity {
             return "";
         }
     }
-
-    private void sendCmd(Socket wSocket,String cmd) throws IOException {
-        OutputStream out = wSocket.getOutputStream();
-
-        out.write((cmd + "\r").getBytes());
-        out.flush();
-    }
-
-    private String readRPMData(Socket wSocket,int index) throws Exception {
+    private int readVoltageData(Socket wSocket,int index) throws Exception {
         List  buffer = new ArrayList<Integer>();
-        Thread.sleep(400);
+        Thread.sleep(100);//eski değer 400//******************
         String rawData = null;
         String value = "";
         InputStream in = wSocket.getInputStream();
@@ -120,14 +119,55 @@ public class MainActivity extends Activity {
             res.append((char) b);
 
         rawData = res.toString().trim();
+        if(!rawData.contains("atrv")){
 
-        if(!rawData.contains("01 0C")){
-            return rawData;
+            return Integer.parseInt(rawData);
+
         }
 
         rawData = rawData.replaceAll("\r", " ");
+        rawData = rawData.replaceAll("atrv", "");
+        rawData = rawData.replaceAll("41 0D"," ").trim();
+        String[] data = rawData.split(" ");
+
+        Log.i("com.example.app", "rawData2: "+rawData);
+        Log.i("com.example.app", "data2: "+data[0]);
+        Log.i("com.example.app", "datawew2: "+Integer.decode("0x" + data[0]));
+        Log.i("com.example.app", "datawew2: "+String.valueOf(Integer.decode("0x" + data[0])));
+
+        return Integer.decode("0x" + data[0]);
+        //return Integer.decode(data[0]);
+    }
+
+    private void sendCmd(Socket wSocket,String cmd) throws IOException {
+        OutputStream out = wSocket.getOutputStream();
+        out.write((cmd + "\r").getBytes());
+        out.flush();
+    }
+
+    private int readRPMData(Socket wSocket,int index) throws Exception {
+        Thread.sleep(100);//eski değer 400//******************
+        String rawData = null;
+        InputStream in = wSocket.getInputStream();
+        byte b = 0;
+        StringBuilder res = new StringBuilder();
+
+        // read until '>' arrives
+        while ((char) (b = (byte) in.read()) != '>')
+            res.append((char) b);
+
+        rawData = res.toString().trim();
+
+        if(!rawData.contains("01 0C")){
+            return Integer.parseInt(rawData.toString());
+        }
+
+        rawData = rawData.replaceAll("\r", " ");
+        System.out.println("rawData Ogrenme : "+rawData);
         rawData = rawData.replaceAll("01 0C", "");
+        System.out.println("rawData Ogrenme : "+rawData);
         rawData = rawData.replaceAll("41 0C"," ").trim();
+        System.out.println("rawData Ogrenme : "+rawData);
         String[] data = rawData.split(" ");
 
         Log.i("com.example.app", "rawData: "+rawData);
@@ -147,12 +187,12 @@ public class MainActivity extends Activity {
         int values = ((a*256)+b1)/4;
 
         Log.i("com.example.app", "values RPM: "+values);
-        return String.valueOf(values);
+        return values;
     }
 
-    private String readSpeedData(Socket wSocket,int index) throws Exception {
+    private Integer readSpeedData(Socket wSocket,int index) throws Exception {
         List  buffer = new ArrayList<Integer>();
-        Thread.sleep(400);
+        Thread.sleep(100);//eski değer 400//******************
         String rawData = null;
         String value = "";
         InputStream in = wSocket.getInputStream();
@@ -166,7 +206,7 @@ public class MainActivity extends Activity {
         rawData = res.toString().trim();
         if(!rawData.contains("01 0D")){
 
-            return rawData;
+            return Integer.parseInt(rawData);
 
         }
 
@@ -180,7 +220,7 @@ public class MainActivity extends Activity {
         Log.i("com.example.app", "datawew: "+Integer.decode("0x" + data[0]));
         Log.i("com.example.app", "datawew: "+String.valueOf(Integer.decode("0x" + data[0])));
 
-        return Integer.decode("0x" + data[0]).toString();
+        return Integer.decode("0x" + data[0]);
     }
 
     @Override
