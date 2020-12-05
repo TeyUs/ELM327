5730b46d15a73a3556093009df052958e993b070
Teyhanytu <teyhanytu@gmail.com>
2020-12-05 16:44:41 +0300

    textFile eklendi sorunlar giderildi

--- app/src/main/java/com/example/iot_obdii/MainActivity.java
+++ app/src/main/java/com/example/iot_obdii/MainActivity.java
@@ -26,10 +26,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    File file;
     Calendar calendar;
-    TextView speedText,voltText, fuelStatusText, coolantTempText, dateTXT, rangeTXT,fuelRatetXT;
-    Integer curCoolant = 0 ,curSpeed = 0, curRPM = 0;
+    TextView speedText, voltText, fuelStatusText, coolantTempText, dateTXT, rangeTXT, fuelRatetXT;
+    Integer curCoolant = 0, curSpeed = 0, curRPM = 0;
     Double curVolt = 0.0, curFuel = 0.0;
     private ProgressBar progressBarRPM;
     private ProgressBar progressBarSpeed;
@@ -51,21 +50,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkExternalMedia();
-
-        File root = android.os.Environment.getExternalStorageDirectory();
-        File dir = new File (root.getAbsolutePath() + "/download");
-        dir.mkdirs();
-        file = new File(dir, "xxxxxxxxxxxxxxxxxxxxxxxx.txt");
-
-        writeToSDFile("sa");
-
         progressBarRPM = (ProgressBar) findViewById(R.id.progressBarRPM);
         progressBarSpeed = (ProgressBar) findViewById(R.id.progressBarSpeed);
         speedText = findViewById(R.id.textView_speed);
-        voltText =findViewById(R.id.textVolt);
-        fuelStatusText =findViewById(R.id.gas_tank);
-        coolantTempText =findViewById(R.id.engine_temperature);
+        voltText = findViewById(R.id.textVolt);
+        fuelStatusText = findViewById(R.id.gas_tank);
+        coolantTempText = findViewById(R.id.engine_temperature);
         dateTXT = findViewById(R.id.dateText);
         rangeTXT = findViewById(R.id.range);
         fuelRatetXT = findViewById(R.id.burn_rate);
@@ -78,45 +68,46 @@ public class MainActivity extends Activity {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_VISIBLE
-                        |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        |View.SYSTEM_UI_FLAG_FULLSCREEN
-                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
 
-    public Double avarageKM(int speedInt){
-        Double speed = (double)speedInt ;
-        speed_Integral = (T_s*(speed + cur_z1_s) + 2 * int_z1_s) / 2;
+    public Double avarageKM(int speedInt) {
+        Double speed = (double) speedInt;
+        speed_Integral = (T_s * (speed + cur_z1_s) + 2 * int_z1_s) / 2;
         cur_z1_s = speed;
         int_z1_s = speed_Integral;
-        return speed_Integral/475.0;
+        return speed_Integral / 475.0;
     }
 
-    public  void setSpeed(String mspeed){
+    public void setSpeed(String mspeed) {
         curSpeed = Integer.parseInt(mspeed);
         totalKM = avarageKM(curSpeed);
-        String m = String.format("%.2f",totalKM);
-        m = m+"KM";
+        String m = String.format("%.2f", totalKM);
+        m = m + "KM";
         rangeTXT.setText(m);
-        if(mspeed.matches("0")) {
+        if (mspeed.matches("0")) {
             isMove = false;
-        }else{
+        } else {
             isMove = true;
         }
         speedText.setText(mspeed);
         int speed = Integer.parseInt(mspeed);
         int kayma = 0;
-        speed +=kayma;
+        speed += kayma;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            progressBarSpeed.setProgress(speed,true);
-        }else{
+            progressBarSpeed.setProgress(speed, true);
+        } else {
             progressBarSpeed.setProgress(speed);
         }
         speedText.setText(mspeed);
     }
-    public  void setRPM(String rpm){
+
+    public void setRPM(String rpm) {
         curRPM = Integer.parseInt(rpm);
         dataBase();
         int kayma = 0;
@@ -124,15 +115,15 @@ public class MainActivity extends Activity {
         rpmInt = (rpmInt * 3) / 100 + kayma;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            progressBarRPM.setProgress(rpmInt,true);
-        }else{
+            progressBarRPM.setProgress(rpmInt, true);
+        } else {
             progressBarRPM.setProgress(rpmInt);
         }
     }
 
-    public  void setVolt(String mvolt){
+    public void setVolt(String mvolt) {
         voltText.setText(mvolt);
-        mvolt=mvolt.replace("V"," ").trim();
+        mvolt = mvolt.replace("V", " ").trim();
         try {
             curVolt = Double.parseDouble(mvolt);
         } catch (Exception e) {
@@ -143,43 +134,43 @@ public class MainActivity extends Activity {
         // Toast.makeText(this, curVolt.toString(), Toast.LENGTH_SHORT).show();
     }
 
-    public  void setFuelStatus(String m){
+    public void setFuelStatus(String m) {
         Double x = Double.parseDouble(m);
 
-        if(isfuel)
-        {
+        if (isfuel) {
             initFuel = x;
             isfuel = false;
         }
 
-        harcanan =+  initFuel - x ;
-        String y = String.format("%.2f",x);
-        fuelStatusText.setText(y +" L");
-        if(isMove) {
+        harcanan = +initFuel - x;
+        String y = String.format("%.2f", x);
+        fuelStatusText.setText(y + " L");
+        if (isMove) {
             Double fuelRate = (harcanan / totalKM);
-            String fuelR = String.format("%.2f",fuelRate);
+            String fuelR = String.format("%.2f", fuelRate);
             fuelRatetXT.setText(fuelR);
         }
         try {
             curFuel = x;
-        }catch (Exception e){
+        } catch (Exception e) {
             Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
-    public  void setcoolantTemp(String m){
-        m = m+" C";
+
+    public void setcoolantTemp(String m) {
+        m = m + " C";
         coolantTempText.setText(m);
 
         try {
             curCoolant = Integer.parseInt(m);
-        }catch (Exception e){
+        } catch (Exception e) {
             Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
 
-            dataBaseRare();
+        dataBaseRare();
     }
 
-    public void init(View view){
+    public void init(View view) {
         //Context context = this.getApplicationContext();
 
         Log.i(DISPLAY_SERVICE, "Button clicked : " + view.getId());
@@ -188,82 +179,67 @@ public class MainActivity extends Activity {
         task.execute();
     }
 
-    public void dataBase(){
-        try{
-            SQLiteDatabase database = openOrCreateDatabase("Data", MODE_PRIVATE,null);
+    public void dataBase() {
+        try {
+            SQLiteDatabase database = openOrCreateDatabase("Data", MODE_PRIVATE, null);
             database.execSQL("CREATE TABLE IF NOT EXISTS data (speed INTEGER, rpm INTEGER)");
-            database.execSQL("INSERT INTO data (speed, rpm) VALUES ("+curSpeed+", "+curRPM+")");
+            database.execSQL("INSERT INTO data (speed, rpm) VALUES (" + curSpeed + ", " + curRPM + ")");
 
             String s = "time : " + calendar.getTimeInMillis() + "speed : " + curSpeed + "RPM : " + curRPM;
-            writeToSDFile(s);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-    public void dataBaseRare(){
-        try{
-            SQLiteDatabase database = openOrCreateDatabase("Data", MODE_PRIVATE,null);
-            database.execSQL("CREATE TABLE IF NOT EXISTS rare (volt FLOAT, fuel INTEGER, coolant INTEGER)");
-            database.execSQL("INSERT INTO rare (volt, fuel, coolant) VALUES ("+curVolt+", "+curFuel+", "+curCoolant+")");
-        }catch (Exception e){
+            writeToFile(s);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void graphIntent(View view){
-        Intent intent = new Intent(MainActivity.this,GraphScreen.class);
-        intent.putExtra("type","speed");
+    public void dataBaseRare() {
+        try {
+            SQLiteDatabase database = openOrCreateDatabase("Data", MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS rare (volt FLOAT, fuel INTEGER, coolant INTEGER)");
+            database.execSQL("INSERT INTO rare (volt, fuel, coolant) VALUES (" + curVolt + ", " + curFuel + ", " + curCoolant + ")");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void graphIntent(View view) {
+        writeToFile("graph'a basıldı. Dosya açıldı mı denemek için\n");
+        Intent intent = new Intent(MainActivity.this, GraphScreen.class);
+        intent.putExtra("type", "speed");
         startActivity(intent);
     }
-    public void setDate(){
+
+    public void setDate() {
         Calendar calendar = Calendar.getInstance();
         String s = calendar.getTime().toString();
-        String [] token = s.split(" ");
-        String date = token[2]+ " " + token[1] + " " + token[5];
-        String [] tokenDate = token[3].split(":");
-        String timeInDay = tokenDate[0]+ ":" + tokenDate[1];
+        String[] token = s.split(" ");
+        String date = token[2] + " " + token[1] + " " + token[5];
+        String[] tokenDate = token[3].split(":");
+        String timeInDay = tokenDate[0] + ":" + tokenDate[1];
         System.out.println(timeInDay);
         System.out.println(date);
         this.dateTXT.setText(date);
     }
 
-    private void writeToSDFile(String s){
+    private void writeToFile(String note) {
+        File dosyaYolu = Environment.getExternalStorageDirectory();
+        File dataFile = new File(dosyaYolu, "Data_File_OBD_ELM327.txt");
         try {
-            FileWriter fileWriter;
-            fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+            if (!dataFile.exists()) {
+                dataFile.createNewFile();
+            }
+            FileWriter fw = new FileWriter(dataFile);
+            BufferedWriter writer = new BufferedWriter(fw);
+            writer.write(note);
+            writer.flush();
+            writer.close();
 
-            bufferedWriter.write(s);
-            bufferedWriter.close();
-            /*
-            FileOutputStream f = new FileOutputStream(file);
-            PrintWriter pw = new PrintWriter(f);
-            pw.println(s);
-            pw.flush();
-            pw.close();
-            f.close();*/
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    private void checkExternalMedia(){
-        boolean mExternalStorageAvailable = false;
-        boolean mExternalStorageWriteable = false;
-        String state = Environment.getExternalStorageState();
 
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            // Can read and write the media
-            mExternalStorageAvailable = mExternalStorageWriteable = true;
-        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            // Can only read the media
-            mExternalStorageAvailable = true;
-            mExternalStorageWriteable = false;
-        } else {
-            // Can't read or write
-            mExternalStorageAvailable = mExternalStorageWriteable = false;
-        }
+
     }
 
 }
